3688a8009315bbbbe6fa67f7aa9563a000a03940
Daniel Mateu <danielmateu86@gmail.com>
2023-02-24 11:24:47 +0100

    Button to change fact added

--- src/App.jsx
+++ src/App.jsx
@@ -11,37 +11,8 @@ export const App = () => {
   const [imageUrl, setImageUrl] = useState()
   // Efecto para obtener el fact al cargar la pagina
   useEffect(() => {
-    // const getCatFact = async () => {
-    //   const response = await fetch(CAT_RANDOM_ENDPOINT_FACT)
-    //   const data = await response.json()
-    //   setCatFact(data.fact)
-    // }
-    // getCatFact()
-    fetch(CAT_RANDOM_ENDPOINT_FACT)
-      .then(response => response.json())
-      // TODO: Handle error if !response.ok
-    // Si no hay respuesta, lanzar nuevo error
-      .catch(error => {
-        throw new Error(error)
-      })
-      .then(data => {
-        const { fact } = data
-        setCatFact(fact)
-        // First word
-        // const firstTrheeWords = fact.split(' ', 3).join(' ')
-        // // console.log(firstTrheeWords)
-        // const catImageUrl = `https://cataas.com/cat/says/${firstTrheeWords}?size=50&color=red&json=true`
-
-        // fetch(catImageUrl)
-        //   .then((response) => response.json())
-        //   .then(data => {
-        //     // console.log(data)
-        //     const { url } = data
-        //     setImageUrl(url)
-        //   })
-      })
+    handleClick()
   }, [])
-
   // Efecto para recuperar la imagen cada vez que tenemos una cita nueva
   useEffect(() => {
     if (!fact) return
@@ -55,11 +26,29 @@ export const App = () => {
     })
   }, [fact])
 
+  const handleClick = () => {
+    // getCatFact()
+    fetch(CAT_RANDOM_ENDPOINT_FACT)
+      .then(response => response.json())
+      // TODO: Handle error if !response.ok
+    // Si no hay respuesta, lanzar nuevo error
+      .catch(error => {
+        throw new Error(error)
+      })
+      .then(data => {
+        const { fact } = data
+        setCatFact(fact)
+      })
+  }
+
   return (
         <main>
             <h1>Random Cat Fact</h1>
+            <button onClick={handleClick}>Get new Cat</button>
+            <section>
             {fact && <p>{fact}</p>}
             {imageUrl && <img src={`${CAT_PREFIX_IMG_URL}${imageUrl}`} alt={`Imagen extraida de las primeras tres letras de ${fact}`} />}
+            </section>
         </main>
   )
 }
